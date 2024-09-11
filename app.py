e56cf0f30464591ea2686e073f5eb2ce4ce7d302
Jared-Anderson-676 <jaredanderson676@gmail.com>
2024-09-11 12:05:38 -0500

    Integrated with ygoprodeck for card data lookup. Also implemented fuzzy string matching for verification.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
+import requests
+from fuzzywuzzy import fuzz
 
 app = Flask(__name__)
 
 #Dummy Test Data
-testCardImageData = [{'imageid': 1, 'data': 'Dark Magician'},
-        {'imageid': 2, 'data': 'Blue-Eyes White Dragon'},
-        {'imageid': 3, 'data': 'Red-Eyes Black Dragon'}]
+testCardImageData = [{'imageid': 1, 'name': 'Dark Magician', 'cardid': '46986414', 'setcode': 'LOB-EN005'},
+        {'imageid': 2, 'name': 'Blue-Eyes White Dragon', 'cardid': '89631139', 'setcode': 'LOB-EN001'},
+        {'imageid': 3, 'name': 'Red-Eyes Black Dragon', 'cardid': '74677422', 'setcode': 'LOB-EN070'}]
 
 testCardContentData = [{'cardID': '46986414', 'card-name': 'Dark Magician', 'card-text': 'The ultimate wizard in terms of attack and defense.', 'atk': 2500, 'def': 2100},
                        {'cardID': '89631139', 'card-name': 'Blue-Eyes White Dragon', 'card-text': 'This legendary dragon is a powerful engine of destruction. Virtualy invincible, very few have faced this awesome creature and lived to tell the tale.', 'atk': 3000, 'def': 2500},
@@ -27,7 +29,7 @@ def post_getCardInfo():
         clientCardInfo = request.get_json()
         card = processCardImage(clientCardInfo)
         if 'error' in card:
-            return {"error": "Card Not Found"}, 415
+            return {"error": card['error']}, 415
         return card, 201
     return {"error": "Request must be JSON"}, 415
 
@@ -35,12 +37,25 @@ def post_getCardInfo():
 
 def processCardImage(cardImageData):
     #Replace enclosed with actual card image parsing
-    cardName=cardImageData['data']
+    cardId = cardImageData['cardid']
+    cardSetCode = cardImageData['setcode']
+    cardName = cardImageData['name']
+    setName = ""
     #End dummy code
-    #ADD YGOPRODECK API LOOKUP HERE
-    for card in testCardContentData:
-        if card['card-name'] == cardName:
-            return card
-    #End dummy lookup
+    url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+    payload = {'id': cardId}
+    resp = requests.get(url, params=payload)
+    if resp.status_code == 200:
+        cardData = resp.json()
+        if fuzz.ratio(cardData['data'][0]['name'], cardName) < 90:
+            print("Fuzzy ratio is: ", fuzz.ratio(cardData['data'][0]['name'], cardName))
+            return {'error': 'card search returned wrong card. please try scanning again'}
+        print("Fuzzy ratio is: ", fuzz.ratio(cardData['data'][0]['name'], cardName))
+        for setData in cardData['data'][0]['card_sets']:
+            if fuzz.ratio(setData['set_code'], cardSetCode) > 95:
+               setName = setData['set_name']
+               break
+        if setName == "":
+            return {'error': 'scanned set code does not match a valid printing. Card could be fake or try scanning again'}
+        return cardData
     return {'error': 'card not found'}    
-
