f9cef9e32f6bdce37860a26e76a9ce40c5dbda4e
Jack <JackAnd356@gmail.com>
2024-10-15 16:01:45 -0400

    Moved python files into backend

---
+++ backend/Cards.py
@@ -0,0 +1,256 @@
+import cv2
+import numpy as np
+import pytesseract
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Threshold Levels
+THRESH_LOW = 0
+THRESH_HIGH = 140
+
+CARD_MAX_AREA = 2000000
+CARD_MIN_AREA = 5000
+
+
+"""Processes the input image and returns a dictionary with a list of all cards in frame, 
+card info is provided as a dictionary within each card"""
+def process_image(image):
+    height, width = image.shape[:2]
+    cards = []
+
+    # Define the new dimensions
+    new_width = 600
+    new_height = int(new_width * height / width)
+
+    # Resize the image
+    image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+    thresh = preprocess_image(image)
+    ccs, isCard = find_cards(thresh)
+
+    if len(ccs) == 0: return None
+
+    for i in range(len(ccs)):
+        if isCard[i]:
+            cards.append(preprocess_card(ccs[i], image, "yugioh"))
+
+    frameCards = dict()
+    frameCards["cards"] = cards
+    return frameCards
+                
+
+
+def preprocess_image(image):
+    """Returns a grayed, blurred, and adaptively thresholded camera image."""
+
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray,(11,11),0)
+
+    threshold1 = THRESH_LOW
+    threshold2 = THRESH_HIGH
+    imgCanny = cv2.Canny(blur, threshold1, threshold2)
+
+
+    kernel = np.ones((5, 5), np.uint8)
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+    
+    return imgDil
+
+def find_cards(thresh_image):
+    """Finds all card-sized contours in a thresholded camera image.
+    Returns the number of cards, and a list of card contours sorted
+    from largest to smallest."""
+
+    # Find contours and sort their indices by contour size
+    cnts,hier = cv2.findContours(thresh_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    index_sort = sorted(range(len(cnts)), key=lambda i : cv2.contourArea(cnts[i]),reverse=True)
+
+    # If there are no contours, do nothing
+    if len(cnts) == 0:
+        return [], []
+    
+    # Otherwise, initialize empty sorted contour and hierarchy lists
+    cnts_sort = []
+    hier_sort = []
+    cnt_is_card = np.zeros(len(cnts),dtype=int)
+
+    # Fill empty lists with sorted contour and sorted hierarchy. Now,
+    # the indices of the contour list still correspond with those of
+    # the hierarchy list. The hierarchy array can be used to check if
+    # the contours have parents or not.
+    for i in index_sort:
+        cnts_sort.append(cnts[i])
+        hier_sort.append(hier[0][i])
+
+    # Determine which of the contours are cards by applying the
+    # following criteria: 1) Smaller area than the maximum card size,
+    # 2), bigger area than the minimum card size, 3) have no parents,
+    # and 4) have four corners
+
+    for i in range(len(cnts_sort)):
+        size = cv2.contourArea(cnts_sort[i])
+        peri = cv2.arcLength(cnts_sort[i],True)
+        approx = cv2.approxPolyDP(cnts_sort[i],0.01*peri,True)
+
+        "print(size)"
+        "print(hier_sort[i][3])"
+        "print(len(approx))"
+        
+        if ((size < CARD_MAX_AREA) and (size > CARD_MIN_AREA)
+            and (hier_sort[i][3] == -1) and (len(approx) == 4)):
+                cnt_is_card[i] = 1
+
+    return cnts_sort, cnt_is_card
+
+def preprocess_card(contour, frame, cardType):
+    qCard = dict()
+    qCard["contour"] = contour
+
+    peri = cv2.arcLength(contour,True)
+    approx = cv2.approxPolyDP(contour,0.01*peri,True)
+    pts = np.float32(approx)
+    qCard["corner_points"] = pts
+
+    # Find width and height of card's bounding rectangle
+    x,y,w,h = cv2.boundingRect(contour)
+    qCard["width"] = w, qCard["height"] = w, h
+
+    # Find center point of card by taking x and y average of the four corners.
+    average = np.sum(pts, axis=0)/len(pts)
+    cent_x = int(average[0][0])
+    cent_y = int(average[0][1])
+    qCard["centerpoint"] = [cent_x, cent_y]
+
+    # Warp card into 200x300 flattened image using perspective transform
+    qCard["warp"] = flattener(frame, pts, w, h)
+
+    if cardType == 'yugioh': (name, id, setCode) = get_yugioh_card_details(qCard["warp"])
+    elif cardType == 'mtg': (name, id, setCode) = get_mtg_card_details(qCard["warp"])
+    qCard["name"] = name
+    qCard["cardid"] = id
+    qCard["setcode"] = setCode
+
+    return qCard
+
+def draw_on_card(qCard, frame):
+    """Draw the card name, center point, and contour on the camera image."""
+
+    x = qCard["centerpoint"][0]
+    y = qCard["centerpoint"][1]
+    cv2.circle(frame,(x,y),5,(255,0,0),-1)
+
+    card_info = qCard["name"] + ", " + qCard["cardid"] + ", " + qCard["setcode"]
+    print(card_info)
+
+    "cv2.putText(frame, card_info, (x,y), font, 1, (255, 0, 0), 2, cv2.LINE_AA)"
+
+    return frame
+
+def get_yugioh_card_details(image):
+    """Use Pytesseract to find the name of card, id of card, and set code of card, by cropping the original image
+       See https://pypi.org/project/pytesseract/"""
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Extract card name
+    nameImg = image[5:60, 10:330]
+    nameImg = cv2.resize(nameImg, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("NameImg", nameImg)
+    cardName = pytesseract.image_to_string(nameImg, config='-c preserve_interword_spaces=1 tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-')
+    
+    # Extract card ID
+    cardIDImg = image[585:598, 5:70]
+    cardIDImg = cv2.resize(cardIDImg, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("cardIDImg", cardIDImg)
+    cardID = pytesseract.image_to_string(cardIDImg, config='--psm 13 -c tessedit_char_whitelist=0123456789')
+    
+    # Extract card set code
+    cardSetCodeImg = image[435:450, 285:380]
+    cardSetCodeImg = cv2.resize(cardSetCodeImg, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("setCodeImg", cardSetCodeImg)
+    cardSetCode = pytesseract.image_to_string(cardSetCodeImg, config='--psm 13 -c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-')
+
+    return (cardName, cardID, cardSetCode)
+
+def get_mtg_card_details(image):
+    """Use Pytesseract to find the name of card, id of card, and set code of card, by cropping the original image
+       See https://pypi.org/project/pytesseract/"""
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Extract card name
+    nameImg = image[20:60, 30:330]
+    nameImg = cv2.resize(nameImg, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("NameImg", nameImg)
+    cardName = pytesseract.image_to_string(nameImg, config='-c preserve_interword_spaces=1 tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-')
+    
+    cardSetCodeImg = image[572:590, 0:70]
+    cardSetCodeImg = cv2.resize(cardSetCodeImg, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("cardSetCodeImg", cardSetCodeImg)
+    cardSetCode = pytesseract.image_to_string(cardSetCodeImg, config='--psm 13 -c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-/')
+
+    # Extract card ID
+    cardIDImg = image[560:572, 0:70]
+    cardIDImg = cv2.resize(cardIDImg, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC)
+    cv2.imshow("cardIDImg", cardIDImg)
+    cardID = pytesseract.image_to_string(cardIDImg, config='--psm 13 -c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-/')
+
+    return (cardName, cardID, cardSetCode)
+
+
+def flattener(image, pts, w, h):
+    """Flattens an image of a card into a top-down 200x300 perspective.
+    Returns the flattened, re-sized, grayed image.
+    See www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"""
+    temp_rect = np.zeros((4,2), dtype = "float32")
+    
+    s = np.sum(pts, axis = 2)
+
+    tl = pts[np.argmin(s)]
+    br = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis = -1)
+    tr = pts[np.argmin(diff)]
+    bl = pts[np.argmax(diff)]
+
+    # Need to create an array listing points in order of
+    # [top left, top right, bottom right, bottom left]
+    # before doing the perspective transform
+
+    if w <= 0.8*h: # If card is vertically oriented
+        temp_rect[0] = tl
+        temp_rect[1] = tr
+        temp_rect[2] = br
+        temp_rect[3] = bl
+
+    if w >= 1.2*h: # If card is horizontally oriented
+        temp_rect[0] = bl
+        temp_rect[1] = tl
+        temp_rect[2] = tr
+        temp_rect[3] = br
+    
+    if w > 0.8*h and w < 1.2*h: #If card is diamond oriented
+        # card is tilted to the left.
+        if pts[1][0][1] <= pts[3][0][1]:
+            temp_rect[0] = pts[1][0] # Top left
+            temp_rect[1] = pts[0][0] # Top right
+            temp_rect[2] = pts[3][0] # Bottom right
+            temp_rect[3] = pts[2][0] # Bottom left
+
+        # card is tilted to the right
+        if pts[1][0][1] > pts[3][0][1]:
+            temp_rect[0] = pts[0][0] # Top left
+            temp_rect[1] = pts[3][0] # Top right
+            temp_rect[2] = pts[2][0] # Bottom right
+            temp_rect[3] = pts[1][0] # Bottom left
+            
+        
+    maxWidth = 400
+    maxHeight = 600
+
+    # Create destination array, calculate perspective transform matrix,
+    # and warp card image
+    dst = np.array([[0,0],[maxWidth-1,0],[maxWidth-1,maxHeight-1],[0, maxHeight-1]], np.float32)
+    M = cv2.getPerspectiveTransform(temp_rect,dst)
+    warp = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    warp = cv2.cvtColor(warp,cv2.COLOR_BGR2RGB)
+
+    return warp
---
+++ backend/classifier.py
@@ -0,0 +1,232 @@
+import requests
+import random
+import os
+import time
+import re
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Input
+import cv2
+from dotenv import load_dotenv, find_dotenv
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+load_dotenv(find_dotenv())
+
+def getYugiohSample(numCards):
+    url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        cardData = resp.json()
+        cards = cardData["data"]
+
+        random.shuffle(cards)
+
+        counter = 0
+
+        for i,card in enumerate(cards):
+            img_url = card["card_images"][0]["image_url"]
+            card_name = card["name"].replace(' ', '_').replace('/', '_')
+            card_name = sanitize_filename(card_name)
+            filename = f'sample_images/yugioh/{card_name}.jpg'
+
+            img = requests.get(img_url)
+            if img.status_code == 200:
+                counter += 1
+                with open(filename, "wb") as img_file:
+                    img_file.write(img.content)
+                print(f'Downloaded {card_name} Num: {i}/{numCards} Total Downloaded: {counter}')
+            else:
+                print(f'Failed to get Image of: {card_name}')
+            if counter >= numCards: break
+            time.sleep(0.25)
+    else:
+        print(f'API Gave Error Status Code: {resp.status_code}')
+
+
+def getMTGSample(numCards):
+    url = 'https://api.scryfall.com/cards/random'
+    headers = {'User-Agent' : 'TCG Card Detection App 0.1', 'Accept' : '*/*'}
+    
+
+    counter = 0
+
+    while counter < numCards:
+        resp = requests.get(url, headers=headers)
+
+        if resp.status_code == 200:
+            cardData = resp.json()
+            if not "image_uris" in cardData: continue
+            img_url = cardData["image_uris"]["large"]
+            card_name = cardData["name"].replace(' ', '_').replace('/', '_')
+            card_name = sanitize_filename(card_name)
+            filename = f'sample_images/mtg/{card_name}.jpg'
+
+            img = requests.get(img_url)
+            if img.status_code == 200:
+                counter += 1
+                with open(filename, "wb") as img_file:
+                    img_file.write(img.content)
+                print(f'Downloaded {card_name} Total Downloaded: {counter}')
+            else:
+                print(f'Failed to get Image of: {card_name}')
+        else:
+            print(f'API Gave Error Status Code: {resp.status_code}')
+        
+        time.sleep(0.25)
+
+def getPokemonSample(numCards):
+    apikey = os.getenv('POKEMON_API_KEY')
+    url = 'https://api.pokemontcg.io/v2/cards'
+    headers = {'X-Api-Key' : apikey}
+    params = {'pageSize' : 250}
+
+    resp = requests.get(url, headers=headers, params=params)
+
+    if resp.status_code == 200:
+        card_data = resp.json()
+        cards = card_data["data"]
+
+        random.shuffle(cards)
+        counter = 0
+
+        for i, card in enumerate(cards):
+            image_url = card['images']['large']
+            card_name = card['name'].replace(' ', '_').replace('/', '_')
+            card_name = sanitize_filename(card_name)
+            filename = f"sample_images/pokemon/{card_name}.jpg"
+
+            img_response = requests.get(image_url)
+
+            if img_response.status_code == 200:
+                counter += 1
+                with open(filename, "wb") as img_file:
+                    img_file.write(img_response.content)
+                print(f"Downloaded: {card_name} Total Downloaded: {counter}")
+            else:
+                print(f"Failed to download image for card: {card_name}")
+            if counter >= numCards: break
+    else:
+        print(f'API Gave Error Status Code: {resp.status_code}')
+
+def getTrainingData():
+    training_images = []
+    training_labels = []
+    test_images = []
+    test_labels = []
+
+    "getYugiohSample(200)"
+    "getMTGSample(200)"
+    "getPokemonSample(200)"
+
+    for i, filename in enumerate(os.listdir("../sample_images/yugioh")):
+        filepath = os.path.join("../sample_images/yugioh", filename)
+        img = cv2.imread(filepath)
+        imgSmall = cv2.resize(img, (128, 128), cv2.INTER_AREA)
+        if i < 150: 
+            training_images.append(imgSmall)
+            training_labels.append(0)
+        else: 
+            test_images.append(imgSmall)
+            test_labels.append(0)
+
+    for i, filename in enumerate(os.listdir("../sample_images/mtg")):
+        filepath = os.path.join("../sample_images/mtg", filename)
+        img = cv2.imread(filepath)
+        imgSmall = cv2.resize(img, (128, 128), cv2.INTER_AREA)
+        if i < 150: 
+            training_images.append(imgSmall)
+            training_labels.append(1)
+        else: 
+            test_images.append(imgSmall)
+            test_labels.append(1)
+
+    for i, filename in enumerate(os.listdir("../sample_images/pokemon")):
+        filepath = os.path.join("../sample_images/pokemon", filename)
+        img = cv2.imread(filepath)
+        imgSmall = cv2.resize(img, (128, 128), cv2.INTER_AREA)
+        if i < 120: 
+            training_images.append(imgSmall)
+            training_labels.append(2)
+        else: 
+            test_images.append(imgSmall)
+            test_labels.append(2)
+
+    training_images = np.array(training_images)
+    training_labels = np.array(training_labels)
+    test_images = np.array(test_images)
+    test_labels = np.array(test_labels)
+
+    return (training_images, training_labels), (test_images, test_labels)
+
+def sanitize_filename(filename):
+    # Remove invalid characters for Windows file names
+    return re.sub(r'[<>:"/\\|?*]', '', filename)
+
+def trainModel(training_data, test_data):
+    model = Sequential([
+        Input(shape=(128, 128, 3)),
+        Conv2D(32, (3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+        
+        Conv2D(64, (3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        Conv2D(128, (3, 3), activation='relu'),
+        MaxPooling2D(pool_size=(2, 2)),
+
+        Flatten(),
+
+        Dense(128, activation='relu'),
+        Dropout(0.5),
+
+        Dense(3, activation='softmax')  # Output layer
+    ])
+
+    model.compile(optimizer='adam',
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'])
+        
+    epochs = 10
+    history = model.fit(
+        training_data[0],
+        training_data[1],
+        validation_data=(test_data[0], test_data[1]),
+        epochs=epochs
+    )
+
+    val_loss, val_acc = model.evaluate(test_data[0], test_data[1])
+    print(f"Validation Accuracy: {val_acc * 100:.2f}%")
+    return model, history
+
+def plot_training_history(history):
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = 10
+    epochs_range = range(epochs)
+
+    plt.figure(figsize=(8, 8))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.show()
+
+training_data, test_data = getTrainingData()
+print(f"Training data: {training_data[0].shape}, Training labels: {training_data[1].shape}")
+print(f"Test data: {test_data[0].shape}, Test labels: {test_data[1].shape}")
+print(test_data[1])
+model, history = trainModel(training_data, test_data)
+plot_training_history(history)
+model.save('card_classifier_model.h5')
