269e13fbee9c4a1686d6716be681a86e9584e1f0
Jared-Anderson-676 <jaredanderson676@gmail.com>
2025-04-29 10:22:35 -0500

    Subcollection calc issues fixed

--- app/src/main/java/com/example/tcgcarddetectionapp/Utils.kt
+++ app/src/main/java/com/example/tcgcarddetectionapp/Utils.kt
@@ -24,18 +24,17 @@ fun <E> List<E>.remove(elem: E): List<E> {
 }
 
 fun updateSubcollectionInfo(subcolInfo: SubcollectionInfo, card: CardData, quantity: Int, adding: Boolean) {
-    val amt = card.subcollections!!.count { it == subcolInfo.subcollectionid}
     if (adding) {
-        if (subcolInfo.totalValue != null) subcolInfo.totalValue = subcolInfo.totalValue!! + (card.price * amt)
-        else subcolInfo.totalValue = (card.price * amt)
+        if (subcolInfo.totalValue != null) subcolInfo.totalValue = subcolInfo.totalValue!! + (card.price * quantity)
+        else subcolInfo.totalValue = (card.price * quantity)
 
-        if (subcolInfo.cardCount != null) subcolInfo.cardCount = subcolInfo.cardCount!! + amt
-        else subcolInfo.cardCount = amt
+        if (subcolInfo.cardCount != null) subcolInfo.cardCount = subcolInfo.cardCount!! + quantity
+        else subcolInfo.cardCount = quantity
     } else {
-        if (subcolInfo.totalValue != null) subcolInfo.totalValue = subcolInfo.totalValue!! - (card.price * amt)
+        if (subcolInfo.totalValue != null) subcolInfo.totalValue = subcolInfo.totalValue!! - (card.price * quantity)
         else println("Removing Card From Subcollection with no Price??")
 
-        if (subcolInfo.cardCount != null) subcolInfo.cardCount = subcolInfo.cardCount!! - amt
+        if (subcolInfo.cardCount != null) subcolInfo.cardCount = subcolInfo.cardCount!! - quantity
         else println("Removing Card from Empty Subcollection??")
     }
 }
